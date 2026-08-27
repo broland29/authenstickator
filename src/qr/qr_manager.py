32326ca45f338c64a165c31bc58bc888832910ea
broland29 <broland29@proton.me>
2026-08-27 19:58:25 +0300

    switch qr reader library to a newer one

--- src/qr/qr_manager.py
+++ src/qr/qr_manager.py
@@ -1,5 +1,5 @@
+import zxingcpp
 from PIL import Image
-from pyzbar.pyzbar import decode
 
 from src.logger.logger_manager import LoggerManager
 
@@ -29,11 +29,10 @@ class QRManager:
         except FileNotFoundError:
             self.logger.error(f"Image {image_path} not found.")
             return None
-        decoded_list = decode(image)  # returns a list, we only care about first, and its data attr.
-        if len(decoded_list) == 0:
+        results = zxingcpp.read_barcodes(image)
+        if len(results) == 0:
             self.logger.error(f"Image {image_path} did not contain any QR codes.")
             return None
-        if len(decoded_list) > 1:
+        if len(results) > 1:
             self.logger.warning(f"Read multiple QR codes, processing only first")
-        decoded_bytes = decoded_list[0].data
-        return decoded_bytes.decode("utf-8")
+        return results[0].text
